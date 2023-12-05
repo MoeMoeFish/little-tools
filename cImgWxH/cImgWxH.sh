895b03d0def3a25f73ac88d879a6b9467f6eef75
moemoefish <moemoefish@qq.com>
2023-12-05 12:52:19 +0800

    feat: parameter check

--- cImgWxH/cImgWxH.sh
+++ cImgWxH/cImgWxH.sh
@@ -1,2 +1,8 @@
+expected_args=2
+if [ $# -ne $expected_args ]; then
+    echo "Usage: $0 [width] [height]"
+    exit 1
+fi
+
 name=$1x$2
 convert -background seagreen3 -fill white -size ${name} -gravity center "caption:${name}" ${name}.png
